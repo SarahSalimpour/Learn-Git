1a66cdf2588d92cc4232a8eb4dabd6da282739a8
Sarah Salimpour <sara1381salimpour@gmail.com>
2022-06-01 11:53:27 +0430

    'computer_chioce added'

---
+++ project2/computer_choice.py
@@ -0,0 +1,7 @@
+def computer_choice(content):
+    '''
+    function that generates a random number based on the available options.
+    returns random int
+    '''
+    computer_chose = randint(0, len(content) - 1)
+    return computer_chose
